5027208c359aea6e09b76c55d212d2899263a096
Ju314ter <julien.feger@gmail.com>
2024-10-01 18:15:38 +0200

    test workflow stripe

--- app/(main)/(context)/actions.ts
+++ app/(main)/(context)/actions.ts
@@ -178,8 +178,9 @@ export async function handleStripeWebhook(formData: FormData) {
 
   if (event.type === "charge.updated") {
     const charge = event.data.object as Stripe.Charge;
-
+    console.log("Charge updated", charge);
     const resultReceiptMail = await sendReceiptEmail(charge);
+    console.log(resultReceiptMail);
     if (!resultReceiptMail.success) {
       console.error(resultReceiptMail.message);
       return { error: resultReceiptMail.message };
@@ -306,6 +307,7 @@ const sendNotificationEmail = async (session: Stripe.Checkout.Session) => {
 
 const sendReceiptEmail = async (charge: Stripe.Charge) => {
   if (!charge.receipt_url || !charge.billing_details?.email) {
+    console.log("Données manquantes pour envoi mail de reçu", charge);
     return {
       success: false,
       message: "Données manquantes pour envoi mail de reçu",
@@ -321,9 +323,9 @@ const sendReceiptEmail = async (charge: Stripe.Charge) => {
 
   const mailOptions: Mail.Options = {
     from: process.env.MY_EMAIL,
-    to: process.env.MY_EMAIL,
+    to: charge.billing_details.email,
     subject: `Dame Pascale: Votre reçu stripe`,
-    text: `Bonjour, votre paiement a été validé, voici votre reçu stripe : ${charge.billing_details?.email}`,
+    text: `Bonjour, votre paiement a été validé, voici votre reçu stripe : ${charge.billing_details.email}`,
   };
 
   const sendMailPromise = () =>
